c18f9d60a9e997a8331a87d697f552be191a179c
MADUSRI0206 <133226655+MADUSRI0206@users.noreply.github.com>
2025-05-28 22:13:32 +0530

    Part Select

---
+++ Assignments/Part_Select.v
@@ -0,0 +1,10 @@
+/*.Part Select Operation
+ Extract lower nibble from reg [7:0] bus using bus[3:0].*/
+
+module part_select_access;
+reg [7:0] bus;
+initial begin
+assign bus=8'b10101100;
+$display(bus[3:0]);
+end
+endmodule
